74da0c2207de8ed2346ab9b224e2e4ff4a3d631e
Priyanka <priyanka.mahadik>
2020-08-13 20:22:02 +0530

    first commit

---
+++ TextValidator/TextValidateViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TextValidateViewController.swift
+//  TextValidator
+//
+//  Created by Sreekanth B on 13/08/20.
+//  Copyright © 2020 Sreekanth B. All rights reserved.
+//
+
+import UIKit
+
+class TextValidateViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
